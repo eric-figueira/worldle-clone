b69f751e522725017f61455edfbfa660b2f64f28
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2026-01-15 15:10:44 -0300

    feat: add haversine distance function

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,2 @@
 export const TOTAL_GUESSES = 6
+export const EARTH_RADIUS_IN_KM = 6371
---
+++ src/lib/distance.ts
@@ -0,0 +1,24 @@
+import { EARTH_RADIUS_IN_KM } from "./constants";
+import type { Country } from "./countries";
+
+export function haversine(from: Country, to: Country) {
+  const radians = {
+    fromLatitude: degreesToRadians(from.latitude),
+    fromLongitude: degreesToRadians(from.longitude),
+    toLatitude: degreesToRadians(to.latitude),
+    toLongitude: degreesToRadians(to.longitude)
+  }
+
+  const a = Math.pow(Math.sin((radians.toLatitude - radians.fromLatitude) / 2), 2)
+  const b = Math.pow(Math.sin((radians.toLongitude - radians.fromLongitude) / 2), 2)
+  const c = Math.cos(radians.fromLatitude) * Math.cos(radians.toLatitude) * b 
+  const d = Math.sqrt(a + c)
+
+  const distance = 2 * EARTH_RADIUS_IN_KM * Math.asin(Math.sqrt(d))
+
+  return distance
+}
+
+export function degreesToRadians(deg: number) {
+  return (deg * 2 * Math.PI) / 360
+}
