580777dcc3fb0ff1c128b7e2fa48a8ceb2534a11
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2026-01-15 15:43:31 -0300

    feat(haversine): add distance percentage function

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,3 @@
 export const TOTAL_GUESSES = 6
 export const EARTH_RADIUS_IN_KM = 6371
+export const MAX_DISTANCE_ON_EARTH_SURFACE_IN_KM = 20000
--- src/lib/distance.ts
+++ src/lib/distance.ts
@@ -1,4 +1,4 @@
-import { EARTH_RADIUS_IN_KM } from "./constants";
+import { EARTH_RADIUS_IN_KM, MAX_DISTANCE_ON_EARTH_SURFACE_IN_KM } from "./constants";
 import type { Country } from "./countries";
 
 export function haversine(from: Country, to: Country) {
@@ -21,4 +21,8 @@ export function haversine(from: Country, to: Country) {
 
 export function degreesToRadians(deg: number) {
   return (deg * 2 * Math.PI) / 360
+}
+
+export function percentage(distance: number) {
+  return Math.round(distance / MAX_DISTANCE_ON_EARTH_SURFACE_IN_KM) * 100
 }
