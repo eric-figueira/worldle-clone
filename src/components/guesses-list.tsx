55f8cb571d2572dad3559823294ab7ec96b2ce2a
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2026-01-17 15:59:13 -0300

    feat: add empty guesses slots

--- src/components/guesses-list.tsx
+++ src/components/guesses-list.tsx
@@ -1,5 +1,6 @@
 import { useGuesses } from "@/hooks/useGuesses"
 import { Guess } from "./guess"
+import { MAX_GUESSES } from "@/lib/constants"
 
 export function GuessesList() {
   const { guesses } = useGuesses()
@@ -9,6 +10,12 @@ export function GuessesList() {
       {guesses.map((guess, i) => {
         return (
           <Guess guess={guess} key={i} />
+        )
+      })}
+
+      {Array.from({ length: MAX_GUESSES - guesses.length }).map((_, i) => {
+        return (
+          <div key={i} className="w-full h-8 bg-slate-50 border-2 border-slate-100 rounded-md text-center text-sm" />
         )
       })}
     </ul>
