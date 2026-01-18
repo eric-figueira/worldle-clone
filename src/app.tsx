6da3fc0fee8f158d5b811f1a30d5b335c0fdf478
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2026-01-18 20:48:35 -0300

    refactor: minor changes

--- src/app.tsx
+++ src/app.tsx
@@ -15,7 +15,7 @@ export function App() {
     <div className="w-full min-h-screen bg-slate-50 p-10">
       <div className="flex flex-col justify-center items-center gap-10 mx-auto max-w-xl bg-white p-8 border border-slate-200 rounded-lg">
         <div className="py-1 bg-slate-100 border-t border-b border-slate-200 w-full">
-          <h1 className="text-2xl font-bold tracking-tight text-slate-900 w-full text-center">Worldle Clone</h1>
+          <h1 className="text-2xl font-bold tracking-tight text-slate-900 w-full text-center">🌎 WOR<span className="text-emerald-500 font-bold">L</span>DLE <span className="font-normal text-slate-500">clone</span></h1>
         </div>
 
         <div className="space-y-10 w-full">
